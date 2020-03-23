a290304c79e03d3501cd1d7697bdc81bc6bd8312
EliasBorngaesser <elias.borngaesser@gmail.com>
2020-03-23 15:57:18 +0100

    beautifying

--- scripts/upload_netcdf.py
+++ scripts/upload_netcdf.py
@@ -12,8 +12,7 @@
 import zipfile
 import requests
 import utils
-import sys,os
-import json,re
+import sys, os
 import logging
 from geoserver.catalog import Catalog
 
@@ -23,6 +22,7 @@ cfg=utils.readConf()
 projectname=cfg['general']['project_name']
 workspace=cfg['geoserver']['workspaceName']
 geoserver_url=cfg['geoserver']['url']
+logging.getLogger().setLevel(cfg['general']['log_level'])
 
 cat=Catalog(geoserver_url+ "/rest/", "admin", "geoserver")
 
@@ -36,23 +36,21 @@ headers_zip = {'content-type': 'application/zip'}
 headers_xml = {'content-type': 'text/xml'}
 netcdfFile=sys.path[0]+'/../outputFiles/'+projectname+'.nc'
 
-#prepare Session
+#prepare Session (Still needed to uploaded NetCDF Datastore (not supported by Geoserver Library))
 session = requests.Session()
 session.auth = ('admin', 'geoserver')
-session.headers.update({'content-type': 'application/json'})#default is json
+
+#assure AdvancedProjectionSetting is turned off (if not -> layers wont display layers correctly)
+r_set_wms_options=session.put(geoserver_url+'/rest/services/wms/settings',
+    data='<wms><metadata><entry key="advancedProjectionHandling">false</entry></metadata></wms>',
+    headers=headers_xml)
+
 #create Workspace if doesn't exist
 if  not cat.get_workspace(workspace):
     cat.create_workspace(workspace,workspace)
 #delete coveragestore with same Name if existing
 if cat.get_store(coveragestore,workspace):
     cat.delete(cat.get_store(coveragestore,workspace),purge="all",recurse=True)
-# r_delete_coveragestore = session.delete(geoserver_url+'/rest/workspaces/'+ workspace  + '/coveragestores/'+coveragestore+'/?recurse=true&purge=all')
-# print(r_delete_coveragestore.content)
-
-#create New Coveragestore
-r_create_coveragestore = session.post(geoserver_url+'/rest/workspaces/'+ workspace  + '/coveragestores?configure=all',
-    data='<coverageStore><name>' + coveragestore + '</name><workspace>' + workspace + '</workspace><enabled>true</enabled><type>NetCDF</type><url>file:' + netcdfFile + '</url></coverageStore>',)
-print(r_create_coveragestore.content)
 
 # zip the nc file into a zip
 zfile = sys.path[0]+'/../outputFiles/data.zip'
@@ -60,28 +58,27 @@ output = zipfile.ZipFile(zfile, 'w')
 output.write(netcdfFile, coveragestore + '.nc', zipfile.ZIP_DEFLATED )
 output.close()
 
-#upload zip file (creates layers automatically)
+#upload zip file (creating coveragestore and layers automatically)
 with open(output.filename, 'rb') as zip_file:
     r_create_layer = session.put(geoserver_url+'/rest/workspaces/' + workspace  + '/coveragestores/' + coveragestore  + '/file.netcdf',
         data=zip_file,
         headers=headers_zip)
-print(r_create_layer.content)
+logging.info('Upload Response: '+str(r_create_layer.status_code))
 os.remove(output.filename)
 
 #assign styles to created layers
 layers= cat.get_layers()
-# r_get_layers =session.get(geoserver_url+'/rest/workspaces/' + workspace  +'/layers')
-# layers = r_get_layers.json()
-# layers = layers['layers']['layer']
 
 for layer in layers:
-    layerName=layer.resource.name
     #GetStyleName
+    layerName=layer.resource.name
+    #Set Stylename
     layer.default_style=layerName
 
-    #check if Style exists
+    #check if Style exists at Geoserver
     if cat.get_style(layer.default_style):
         cat.delete(cat.get_style(layer.default_style))
+    #create New Style from prebuild XML File
     f = open(sys.path[0]+'/../outputFiles/styles/'+layer.default_style+'.xml')
     cat.create_style(layer.default_style, f.read())
     cat.save(layer)
@@ -91,5 +88,5 @@ for layer in layers:
     timeInfo = DimensionInfo("time", "true", "LIST", None, "ISO8601", None)
     coverage.metadata = ({'time': timeInfo})
     cat.save(coverage)
-print('App can be started with: '+cfg['frontend']['absolutePath']+'/index.html')
+logging.info('App can be started at: '+cfg['frontend']['absolutePath']+'/index.html')
   
