31f97ea83527a849160bc398785788d41db9f121
EliasBorngaesser <elias.borngaesser@gmail.com>
2020-03-26 00:24:52 +0100

    moved demo.sh

--- demo.sh
+++ demo.sh
@@ -4,9 +4,9 @@ if [ -z "$1"]; then
     exit
 fi
 export CONFIGFILE=$1
-python scripts/create_netcdf.py
+python /usr/src/backend/scripts/create_netcdf.py
 ret=$?
 if [ $ret -ne 0 ]; then
      exit
 fi  
-python scripts/upload_netcdf.py   
+python /usr/src/backend/scripts/upload_netcdf.py   
